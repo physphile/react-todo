101c66a4b3e3183b4f8c37665f6e3187265d4338
Artem Netsvetaev <physphile@vk.com>
2022-11-04 22:40:14 +0300

    LAYOUT: add <details> tags

--- src/UI/SuperButtonIcon.tsx
+++ src/UI/SuperButtonIcon.tsx
@@ -3,13 +3,14 @@ import "./SuperButtonIcon.css"
 
 interface SuperButtonIconProps {
     iconName: string,
-    onClick: MouseEventHandler<HTMLButtonElement>
+    onClick?: MouseEventHandler<HTMLButtonElement>,
+    className?: string
 }
 
-export default function SuperButtonIcon({iconName, onClick}: SuperButtonIconProps) {
+export default function SuperButtonIcon({iconName, onClick = () => {}, className = ""}: SuperButtonIconProps) {
     return (
-        <button onClick={onClick} className="super-button">
-             <span className="material-symbols-outlined" >
+        <button onClick={onClick} className={className + " super-button"}>
+             <span className="material-symbols-outlined" style={{color: "inherit"}}>
                     {iconName}
                 </span>
         </button>
--- src/components/TasksListItem.tsx
+++ src/components/TasksListItem.tsx
@@ -3,6 +3,7 @@ import "./TasksListItem.css"
 import SuperButtonIcon from "../UI/SuperButtonIcon";
 import {useRecoilState} from "recoil";
 import TasksList from "../store/store";
+import {useState} from "react";
 
 interface TasksListItemProps {
     task: Task
@@ -13,10 +14,31 @@ export default function TasksListItem({task}: TasksListItemProps) {
     const deleteTask = (id: number) => {
         setTasksList(tasksList.filter((task) => task.id !== id));
     }
+    const [summaryIcon, setSummaryIcon] = useState("expand_more")
+    const changeSummaryIcon = () => {
+        const newSummaryIcon = summaryIcon === "expand_more" ? "expand_less" : "expand_more";
+        setSummaryIcon(newSummaryIcon);
+    }
+
     return (
-        <div className="tasks-list-item">
-            <h3>{task.title}</h3>
-            <SuperButtonIcon iconName="delete" onClick={() => deleteTask(task.id)}/>
-        </div>
+        <details className="tasks-list-item-details">
+            <summary className="tasks-list-item-summary" onClick={changeSummaryIcon}>
+                <SuperButtonIcon iconName={summaryIcon} onClick={() => {}} className={'no-pointer-events high-emphasis'}/>
+                <h3 className="tasks-list-item-title">{task.title}</h3>
+                <SuperButtonIcon iconName="delete" onClick={() => deleteTask(task.id)} className={'helper'}/>
+            </summary>
+            <table className="tasks-list-item-info-table">
+                <tr>
+                    <td className="tasks-list-item-info-cell">Created:</td>
+                    <td className="tasks-list-item-info-cell">22.10.2022 14:58</td>
+                </tr>
+                <tr>
+                    <td className="tasks-list-item-info-cell">Done:</td>
+                    <td className="tasks-list-item-info-cell">28.10.2022 20:22</td>
+                </tr>
+            </table>
+
+        </details>
+
     )
 }
