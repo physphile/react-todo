d38cfc026c642e5fb1a2f6a1a1da5bb5647f6429
Artem Netsvetaev <physphile@vk.com>
2022-11-04 15:21:47 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { createElement as e, useState} from 'react';
+import {Product} from "./components/Product"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto max-w-2xl pt-5">
+      <Product></Product>
     </div>
-  );
+  )
 }
 
 export default App;
